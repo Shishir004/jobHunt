adfb734c1676cd667fd9c97d1a516c18866f5826
Shishir004 <shishirtiwari138@gmail.com>
2025-08-19 23:54:05 +0530

    commited for backend

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const applicationRoute=require('./routes/application.route')
 const PORT=process.env.PORT || 3000;
 
 const corsOptions={
-    origin:'http://localhost:5173',
+    origin:['http://localhost:5173',"https://job-hunt-i5te.vercel.app"],
     credentials:true
 }
 app.use(cors(corsOptions));
